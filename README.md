1cb25f16a930d23d153f74ad3c8f7c304dba4645
xmc473@jsti.com <xmc473@jsti.com>
2023-03-12 17:09:15 +0800

    jse8 参考

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 # 参考https://www.yiibai.com/javafx/ 整合而成的 demo
 # 登录部分 参考 https://blog.csdn.net/qq_34391460/category_12192921.html 部分整合的demo
 #  浏览器部分参考：https://www.jianshu.com/p/e90e1ebc417c
+# jse8 部分参考https://docs.oracle.com/javase/8/javafx/get-started-tutorial/hello_world.htm
---
+++ src/jse8/HelloWorld4.java
@@ -0,0 +1,31 @@
+package jse8;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class HelloWorld4 extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("fxml_example.fxml"));
+
+        Scene scene = new Scene(root, 300, 275);
+
+        stage.setTitle("FXML Welcome");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
